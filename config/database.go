8f13079e10050feb1a8cd49fdb52e2aa4609f66d
fizzday <fizzday@yeah.net>
2019-01-04 16:29:53 +0800

    demo

--- config/database.go
+++ config/database.go
@@ -7,6 +7,6 @@ var DbConfig = &gorose.DbConfigSingle{
 	EnableQueryLog:  true,    // 是否开启sql日志
 	SetMaxOpenConns: 0,       // (连接池)最大打开的连接数，默认值为0表示不限制
 	SetMaxIdleConns: 0,       // (连接池)闲置的连接数
-	Prefix:          "gp_", // 表前缀
+	Prefix:          "", // 表前缀
 	Dsn:             "root:root@tcp(localhost:3306)/test?charset=utf8", // 数据库链接
 }
